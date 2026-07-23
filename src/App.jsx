32b6db5f8ff17f47b85bd7fade6dd4e9849fe18f
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-07-24 01:40:21 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,108 @@
-// import './App.css'
 
 
 
-// function App(){
-//   return (
-//     <>
-//     {/* <h1 style={{
-//       textAlign: 'center',
-//       backgroundColor: 'red',
-//       color: 'white',
-//       padding: '20px'
-//     }}>Hello world</h1> */}
-//     <h1 className="heading">Hello world</h1>
-//     </>
-//   )
-// }
 
-// export default App
+import React from 'react'
+import './App.css'
+import Card from './component/Card.jsx'
+import Btn from './component/Btn'
 
 
-
-// import React from 'react'
-// import Card from './component/Card.jsx'
-// import Btn from './component/Btn'
-
-// const App = () => {
-//   const testing = () => {
-//     console.log("props testing");
-
-//   }
-//   return (
-//     <>
-//       <Btn title="hide me" color={"red"} func={testing} />
-//       <h1>App</h1>
-//       <div style={{
-//         display: 'flex'
-//       }}>
-//         <Card title="this is my title" description="this is my card description" src="https://picsum.photos/seed/picsum/200/300" />
-//         <Card title="this is my second title" description="this is my second card description" />
-//       </div>
-//       <Btn title="click me" func={() => console.log("hello world")} />
-//       {/* <Card ></Card> */}
-//     </>
-//   )
-// }
-
-// export default App
-
-import { useState } from "react"
-import Card from "./component/Card"
-
-const App = () => {
-  const [showCard, setShowCard] = useState(false)
+function App() {
   return (
     <>
-      <h1>Use Effect</h1>
-      <button onClick={() => setShowCard(!showCard)}>{showCard ? "Hide" : "Show"} card</button>
-      {showCard ?  <Card title="this is my title" description="this is my card description" /> : null}
+     <h1 className="heading">🍎 Apple Store</h1>
 
+      <div className="container">
+        {/* Card 1 */}
+      <div className="card-box">
+          <Card
+            title="iPhone 15 Pro Max JV"
+            description="Quetta, Pakistan"
+            image="https://i.guim.co.uk/img/media/18badfc0b64b09f917fd14bbe47d73fd92feeb27/189_335_5080_3048/master/5080.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=1562112c7a64da36ae0a5e75075a0d12"
+          />
+
+          <div style={{ padding: "0 20px" }}>
+            <Btn
+              title="Buy Now"
+              color="#0071e3"
+              func={() => console.log("Buy Now")}
+            />
+          </div>
+        </div>
+
+        {/* Card 2 */}
+       <div className="card-box">
+          <Card
+            title="iPhone 17 Pro Max JV"
+            description="Karachi, Pakistan"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpadU03AzuMfKzshnlYVUDuwJo9G_FJdwyCzBMjLGqoM7iBDO7El4xTbCt&s=10"
+          />
+
+          <div style={{ padding: "0 20px" }}>
+            <Btn
+              title="Buy Now"
+              color="#0071e3"
+              func={() => console.log("Buy Now")}
+            />
+          </div>
+        </div>
+
+        {/* Card 3 */}
+    <div className="card-box">
+          <Card
+            title="iPhone 11 Pro Max JV"
+            description="Lahore, Pakistan"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVb5sLD8JGxGuQL75yoS2nZ4ryuVnFPdYVROoCe4qIbs_ZtSN8qrBB4g3e&s=10"
+          />
+
+          <div style={{ padding: "0 20px" }} >
+            <Btn
+              title="Buy Now"
+              color="#0071e3"
+              func={() => console.log("Buy Now")}
+            />
+          </div>
+        </div>
+
+        {/* Card 4 */}
+      <div className="card-box">
+          <Card
+            title="iPhone 13 Pro Max JV"
+            description="Islamabad, Pakistan"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkWFyWUv3meWKQjdKex7r_SYd6RK8Ey_Xtkjvp9WVUtGE1zwAuIsRFv6af&s=10"
+          />
+
+          <div style={{ padding: "0 20px" }}>
+            <Btn
+              title="Buy Now"
+              color="#0071e3"
+              func={() => console.log("Buy Now")}
+            />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,43 +1,50 @@
-import { useEffect, useState } from "react"
-
-const Card = (props) => {
-  const [counterOne, setCounterOne] = useState(0)
-  const [counterTwo, setCounterTwo] = useState(100)
-  // [] dependency array or ya optional haa.
-  useEffect(() => {
-    console.log("component mounted")
-
-    // return () => {
-    //   console.log("component unmount");
-
-    // }
-  } , [counterTwo])
+const Card = ({ title, description, image }) => {
   return (
-    <div style={{
-      margin: '10px 3px',
-      border: '1px solid black',
-      padding: '10px',
-      borderRadius: '5px'
-    }}>
-      <h1>Counter one {counterOne}</h1>
-      <button onClick={()=>setCounterOne(counterOne + 1)}>add counter one</button>
+    <div
+      style={{
+        width: "300px",
+        border: "1px solid #ccc",
+        borderRadius: "10px",
+        padding: "15px",
+        textAlign: "center",
+      }}
+    >
+      <img
+        src={image}
+        alt={title}
+        style={{
+          width: "100%",
+          borderRadius: "10px",
+        }}
+      />
 
-      <br /><br />
-      <h1>Counter two {counterTwo}</h1>
-      <button  onClick={()=>setCounterTwo(counterTwo + 5)}>add counter two</button>
-      {/* <img src={props.src} alt="image" /> */}
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <h2>{title}</h2>
+      <p>{description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Card
-
-
-
+export default Card;
 // use effect ma jab dependency array nahi dainga to har state change pa use effect run hoga.
 
 // use effect ma agar dependency array dainga to sirf aik baar component mount hona pa run hoga.
 
 // agar kisi state ka change pa use effect run krwana ha to us dependency array ma wo state likhdo.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
